6502e349fda449547a800d4353d39c4c57a0d390
Hyon Lee <hyonmail@gmail.com>
2014-11-04 17:55:44 -0800

    Added fixture data

---
+++ app/Global.java
@@ -0,0 +1,26 @@
+import com.avaje.ebean.Ebean;
+import models.Vendor;
+import play.*;
+import play.libs.Yaml;
+
+import java.util.List;
+import java.util.Map;
+
+public class Global extends GlobalSettings {
+
+    @Override
+    public void onStart(Application app) {
+        InitialData.insert(app);
+    }
+
+    static class InitialData {
+        public static void insert(Application app) {
+            if(Ebean.find(Vendor.class).findRowCount() == 0) {
+                Map<String, List<Object>> all = (Map<String, List<Object>>)Yaml.load("initial-data.yml");
+                Ebean.save(all.get("vendors"));
+                Ebean.save(all.get("parts"));
+            }
+        }
+    }
+
+}
